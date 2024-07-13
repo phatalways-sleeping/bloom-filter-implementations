bf2738da661d37061bf77f8ea38cc4b7f65e99be
phatalways_sleeping <trankim147@gmail.com>
2024-07-13 22:43:22 +0700

    feat(code): add interactive demonstration for spelling checker

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use std::io::{stdin, stdout, Write};
+
 use spell_checker_bloom_filters::{
     bloom_filters::BloomFilter,
     spell_checker::{LocalStorage, SpellChecker},
 };
 
 fn main() {
+    let spelling_checker = init_spelling_checker();
+
+    let mut input = String::new();
+
+    print!("Write something: ");
+    
+    stdout().flush().unwrap();
+    
+    stdin().read_line(&mut input).expect("Failed to read line");
+
+    let words = input.split_whitespace();
+
+    for word in words {
+        if !spelling_checker.check_spelling_of(word).unwrap() {
+            println!("{} may have been wrongly typed", word);
+        }
+    }
+}
+
+fn init_spelling_checker() -> SpellChecker {
     let buffer = BloomFilter::builder().build().unwrap();
 
     let database = LocalStorage::builder()
@@ -11,26 +33,9 @@ fn main() {
         .build()
         .unwrap();
 
-    let spell_checker = SpellChecker::builder()
+    SpellChecker::builder()
         .with_buffer(buffer)
         .with_database(Box::new(database))
         .build()
-        .unwrap();
-
-    let words = vec![
-        "hot",
-        "cold",
-        "helo",
-        "catastrophic",
-        "coding",
-        "challenges",
-        "imadethis",
-    ];
-
-    for word in words {
-        let result = spell_checker.check_spelling_of(word).unwrap();
-        if !result {
-            println!("Word {} has spelling errors", word);
-        }
-    }
+        .unwrap()
 }
